3b0536c335c8337139106f142936ecd8358d606c
Mateus Castro <matcastro2004@yahoo.com.br>
2022-05-23 14:13:18 -0300

    Utilizando mutations para inserir, atualizar e remover dados.

--- Construindo API/api/user/resolvers/userResolvers.js
+++ Construindo API/api/user/resolvers/userResolvers.js
@@ -2,6 +2,11 @@ const userResolvers = {
   Query: {
     users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
     user: (root, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id)
+  },
+  Mutation: {
+    adicionaUser: (root, user, { dataSources }) => dataSources.usersAPI.adicionaUser(user),
+    atualizaUser: (root, novosDados, { dataSources }) => dataSources.usersAPI.atualizaUser(novosDados),
+    deletaUser: (root, { id }, { dataSources }) => dataSources.usersAPI.deletaUser(id)
   }
 }
 
